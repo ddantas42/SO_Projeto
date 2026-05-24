ee33fd3109f303f201ff5263b86d937fd14757cc
Diogo-Laptop <YOUR_NOREPLY_EMAIL>
2026-05-24 09:05:10 +0100

    main clean

--- src/lib/macros.h
+++ src/lib/macros.h
@@ -1,6 +1,9 @@
 #ifndef MACROS_H
 #define MACROS_H
 
-# define STORAGE_CAPACITY 10
+# define STORAGE_CAPACITY 10 // Capacidade máxima de armazenamento máximo do tabuleiro onde os drones depositam
+# define NUMBER_OF_DRONES 3 // Números total de drones a serem criados
+# define DRONE_FINDING_TIME 5 // Tempo que o drone leva para encontrar um item (em segundos)
+
 
 #endif
